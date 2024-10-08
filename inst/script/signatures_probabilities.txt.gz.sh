47c91e8c609b9346bb1eb1d876d8ae3396ae9116
Michela Dell'Alma <64406165+micheladellalma@users.noreply.github.com>
2024-10-08 11:40:15 +0200

    Add files via upload

---
+++ inst/script/signatures_probabilities.txt.gz.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# the .txt data file containing 30 signatures probabilities can be downloaded from:
+
+wget -nd https://cancer.sanger.ac.uk/cancergenome/assets/signatures_probabilities.txt
